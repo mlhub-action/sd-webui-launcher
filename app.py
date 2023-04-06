d0c82a48c93ff8c71a2739109ad1ac6f705457fe
sumof2primes <sumof2primes@gmail.com>
2023-04-06 20:49:22 +0900

    - mmcv 패키지 버전 업데이트로 ddetailer 확장 설치 안되는 문제 수정  - ddetailer 의존 패키지 버전 명시 : openmim==0.3.7, mmcv-full==1.7.1, mmdet==2.28.2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # @title ## 2. 런처 앱 ##
-VERSION = "v0.3.7"  # @param {type:"string"}
+VERSION = "v0.3.8"  # @param {type:"string"}
 
 # @markdown ## <br> 런처 웹페이지 표시 방법 선택 ##
 # @markdown - 체크시(기본값) : 웹 브라우저 창에 표시(🐢응답 <font color="red">느림</font>, 👍보기 <font color="blue">편안</font>)<br>
